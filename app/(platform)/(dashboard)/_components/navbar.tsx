f31ebca5db90046151aa1ce2daf134dc92ca432d
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-26 11:31:58 +0000

    org navbar created

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,57 @@
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed z-50 top-0 w-full px-4 h-14 border-b shadow-sm bg-white flex items-center">
+      {/* TODO: Mobile Sidebar */}
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+
+        <Button
+          size="sm"
+          className="rounded-sm md:flex md:gap-x-1 h-auto py-1.5 px-2"
+        >
+          <Plus className="h-4 w-4" />
+          <span className="hidden md:block">Create</span>
+        </Button>
+      </div>
+
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          afterSelectOrganizationUrl="/organization/:id"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+import { PropsWithChildren } from "react";
+import Navbar from "./_components/navbar";
+
+const DashboardLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="h-full">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
