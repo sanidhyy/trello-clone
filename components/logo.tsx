0214799af7161f5bddb69c95fb2ebf33094f4e2e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-26 09:45:48 +0000

    logo created

---
+++ components/logo.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.woff2",
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image
+          src="/logo.svg"
+          alt="taskify logo"
+          height={30}
+          width={30}
+          aria-hidden
+        />
+
+        <p
+          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
+        >
+          Taskify
+        </p>
+      </div>
+    </Link>
+  );
+};
