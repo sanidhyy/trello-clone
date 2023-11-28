f0fc49e0ed80740026ca34372aa3a95273d7231a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-28 08:28:43 +0000

    create board created

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -0,0 +1,31 @@
+import { HelpCircle, User2 } from "lucide-react";
+
+import { Hint } from "@/components/hint";
+
+export const BoardList = () => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center font-semibold text-lg text-neutral-700">
+        <User2 className="h-6 w-6 mr-2" />
+        Your boards
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div
+          role="button"
+          className="aspect-video relative h-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+        >
+          <p className="text-sm">Create new board</p>
+          <span className="text-xs">5 remaining</span>
+
+          <Hint
+            sideOffset={40}
+            description={`Free workspaces can have upto 5 open boards. For unlimited boards, please upgrade this workspace.`}
+          >
+            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+          </Hint>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,18 @@
+import { Separator } from "@/components/ui/separator";
+
 import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 
 const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
+
+      <Separator className="my-4" />
+
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
