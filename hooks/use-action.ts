be8cea2059c4b25e9de98f91d292c555a6cecf95
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-28 13:19:23 +0530

    error typo fixed

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -35,7 +35,7 @@ export const useAction = <TInput, TOutput>(
 
         if (!result) return;
 
-        if (result.fieldErrors) setFieldErrors(result.fieldErrors);
+        setFieldErrors(result.fieldErrors);
 
         if (result.error) {
           setError(result.error);
