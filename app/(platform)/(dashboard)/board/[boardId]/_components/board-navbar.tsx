96630629fe1dd11efb25780e053a510b9efeace4
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-28 12:41:45 +0000

    board options created

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,5 +1,7 @@
 import { Board } from "@prisma/client";
+
 import { BoardTitleForm } from "./board-title-form";
+import { BoardOptions } from "./board-options";
 
 type BoardNavbarProps = {
   data: Board;
@@ -9,6 +11,10 @@ export const BoardNavbar = async ({ data }: BoardNavbarProps) => {
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
       <BoardTitleForm data={data} />
+
+      <div className="ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   );
 };
---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { MoreHorizontal, X } from "lucide-react";
+
+type BoardOptionsProps = {
+  id: string;
+};
+
+export const BoardOptions = ({ id }: BoardOptionsProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-auto w-auto p-2" variant="transparent">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm font-medium text-center text-neutral-600">
+          Board actions
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+
+        <Button
+          variant="ghost"
+          onClick={() => {}}
+          className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+        >
+          Delete this board
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+};
