d961134461aa5b8b7d877bd1dbd7e2db0a970d0c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-28 11:38:01 +0000

    dynamic title added

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,6 +1,16 @@
 import { PropsWithChildren } from "react";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
 
 import { OrgControl } from "./_components/org-control";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "Organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: PropsWithChildren) => {
   return (
