a32db5e19a6f99dcc93cd8ca28532be3472f4f34
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-28 12:18:21 +0000

    custom btn variant added

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -21,6 +21,7 @@ const buttonVariants = cva(
 
         // custom
         primary: "bg-sky-700 text-primary-foreground hover:bg-sky-700/90",
+        transparent: "bg-transparent text-white hover:bg-white/20",
       },
       size: {
         default: "h-10 px-4 py-2",
