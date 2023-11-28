b35e16dabc23d6e7976d698451a494702232ea32
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-28 12:35:49 +0530

    create board abstraction implemented

---
+++ actions/create-board/index.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { auth } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { InputType, ReturnType } from "@/actions/create-board/types";
+import { CreateBoard } from "@/actions/create-board/schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title } = data;
+
+  let board;
+
+  try {
+    board = await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to create",
+    };
+  }
+
+  revalidatePath(`/board/${board.id}`);
+  return { data: board };
+};
+
+export const createBoard = createSafeAction(CreateBoard, handler);
---
+++ actions/create-board/schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const CreateBoard = z.object({
+  title: z
+    .string({
+      required_error: "Title is required.",
+      invalid_type_error: "Title is required.",
+    })
+    .min(3, {
+      message: "Title is too short.",
+    }),
+});
