a81e035b59054d25ecf88bbf82a9b626a384be46
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-29 13:21:09 +0000

    drag n drop added in list items

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Card } from "@prisma/client";
+import { Draggable } from "@hello-pangea/dnd";
 
 type CardItemProps = {
   data: Card;
@@ -9,11 +10,18 @@ type CardItemProps = {
 
 export const CardItem = ({ data, index }: CardItemProps) => {
   return (
-    <div
-      role="button"
-      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
-    >
-      {data.title}
-    </div>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   );
 };
