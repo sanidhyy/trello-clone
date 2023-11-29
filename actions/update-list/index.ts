42e9b6690eeb7c81f5d6561998f8f2f4bfafcbe8
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-29 10:57:00 +0000

    list update action done

---
+++ actions/update-list/index.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { auth } from "@clerk/nextjs";
+
+import { UpdateList } from "./schema";
+import { InputType, ReturnType } from "./types";
+import { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title, id, boardId } = data;
+
+  let list;
+
+  try {
+    list = await db.list.update({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        },
+      },
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to update.",
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return {
+    data: list,
+  };
+};
+
+export const updateList = createSafeAction(UpdateList, handler);
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useState, useEffect, useRef, ElementRef } from "react";
+import { toast } from "sonner";
 import { useEventListener } from "usehooks-ts";
 import { List } from "@prisma/client";
 
 import { FormInput } from "@/components/form/form-input";
+import { useAction } from "@/hooks/use-action";
+import { updateList } from "@/actions/update-list";
 
 type ListHeaderProps = {
   data: List;
@@ -29,6 +32,35 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
     setIsEditing(false);
   };
 
+  const { execute } = useAction(updateList, {
+    onSuccess: (data) => {
+      toast.success(`Renamed to "${data.title}"`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    if (title === data.title) return disableEditing();
+
+    execute({
+      title,
+      id,
+      boardId,
+    });
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === "Escape") {
       formRef.current?.requestSubmit();
@@ -40,7 +72,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
       {isEditing ? (
-        <form action="" className="flex-1 px-[2px]">
+        <form ref={formRef} action={onSubmit} className="flex-1 px-[2px]">
           <input
             type="hidden"
             id="id"
@@ -59,12 +91,13 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
           />
           <FormInput
             ref={inputRef}
-            onBlur={() => {}}
+            onBlur={onBlur}
             id="title"
             placeholder="Enter list title.."
             defaultValue={title}
             className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
           />
+          <button type="button" hidden aria-disabled />
         </form>
       ) : (
         <div
