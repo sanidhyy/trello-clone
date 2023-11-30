2a7972e2ae183c35b42042db18a93e85e7626835
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-30 16:13:59 +0000

    board count decrement on delete board action

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
@@ -8,8 +9,8 @@ import { DeleteBoard } from "./schema";
 import { InputType, ReturnType } from "./types";
 import { db } from "@/lib/db";
 import { createSafeAction } from "@/lib/create-safe-action";
-import { redirect } from "next/navigation";
 import { createAuditLog } from "@/lib/create-audit-log";
+import { decreaseAvailableCount } from "@/lib/org-limit";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -31,6 +32,9 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         orgId,
       },
     });
+
+    // decrement board count/increase board limit
+    await decreaseAvailableCount();
 
     // create new activity log
     await createAuditLog({
