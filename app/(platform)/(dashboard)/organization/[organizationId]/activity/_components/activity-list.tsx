91c0e84a3ab7ff35f8f79d90edc15301f7378459
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-30 14:09:37 +0000

    activity list page done

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
@@ -0,0 +1,43 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { ActivityItem } from "@/components/activity-item";
+import { db } from "@/lib/db";
+
+export const ActivityList = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) return redirect("/select-org");
+
+  const auditLogs = await db.auditLog.findMany({
+    where: {
+      orgId,
+    },
+  });
+
+  return (
+    <ol className="space-y-4 mt-4">
+      <p className="hidden last:block text-xs text-center text-muted-foreground">
+        No activity found inside this organization.
+      </p>
+
+      {auditLogs.map((log) => (
+        <ActivityItem key={log.id} data={log} />
+      ))}
+    </ol>
+  );
+};
+
+ActivityList.Skeleton = function ActivityListSkeleton() {
+  return (
+    <ol className="space-y-4 mt-4">
+      <Skeleton className="w-[80%] h-14" />
+      <Skeleton className="w-[50%] h-14" />
+      <Skeleton className="w-[70%] h-14" />
+      <Skeleton className="w-[80%] h-14" />
+      <Skeleton className="w-[75%] h-14" />
+    </ol>
+  );
+};
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -0,0 +1,20 @@
+import { Suspense } from "react";
+
+import { Separator } from "@/components/ui/separator";
+
+import { Info } from "../_components/info";
+import { ActivityList } from "./_components/activity-list";
+
+const ActivityPage = () => {
+  return (
+    <div className="w-full">
+      <Info />
+      <Separator className="my-2" />
+      <Suspense fallback={<ActivityList.Skeleton />}>
+        <ActivityList />
+      </Suspense>
+    </div>
+  );
+};
+
+export default ActivityPage;
