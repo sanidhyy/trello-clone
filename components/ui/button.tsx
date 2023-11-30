e1a43e093dcdd7e045c42ef55420bf8e085c58fa
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-30 11:12:15 +0000

    custom vairant and size added

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -22,12 +22,16 @@ const buttonVariants = cva(
         // custom
         primary: "bg-sky-700 text-primary-foreground hover:bg-sky-700/90",
         transparent: "bg-transparent text-white hover:bg-white/20",
+        gray: "bg-neutral-200 text-secondary-foreground hover:bg-neutral-300",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+
+        // custom
+        inline: "h-auto px-2 py-1.5 text-sm",
       },
     },
     defaultVariants: {
