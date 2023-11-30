45dbdf9a91fdf9e32f557a4461c3cdb0bea9e962
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-30 12:18:16 +0000

    create audit log util created

---
+++ lib/create-audit-log.ts
@@ -0,0 +1,37 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import type { ACTION, ENTITY_TYPE } from "@prisma/client";
+
+import { db } from "@/lib/db";
+
+type Props = {
+  entityId: string;
+  entityType: ENTITY_TYPE;
+  entityTitle: string;
+  action: ACTION;
+};
+
+export const createAuditLog = async (props: Props) => {
+  try {
+    const { orgId } = auth();
+    const user = await currentUser();
+
+    if (!user || !orgId) throw new Error("User not found.");
+
+    const { entityId, entityType, entityTitle, action } = props;
+
+    await db.auditLog.create({
+      data: {
+        orgId,
+        entityId,
+        entityType,
+        entityTitle,
+        action,
+        userId: user.id,
+        userImage: user?.imageUrl,
+        userName: `${user?.firstName}${user?.lastName && ` ${user.lastName}`}`,
+      },
+    });
+  } catch (error) {
+    console.log(`[AUDIT_LOG_ERROR]`, error);
+  }
+};
