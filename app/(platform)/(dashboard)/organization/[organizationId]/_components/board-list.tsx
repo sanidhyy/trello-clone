fd72f61bfe8286b890f31887f4db27f34a3d076b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-30 15:01:11 +0000

    orglimit counts calculated

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -8,6 +8,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Hint } from "@/components/hint";
 import { FormPopover } from "@/components/form/form-popover";
 import { db } from "@/lib/db";
+import { getAvailableCount } from "@/lib/org-limit";
+import { MAX_FREE_BOARDS } from "@/constants/boards";
 
 export const BoardList = async () => {
   const { orgId } = auth();
@@ -22,6 +24,8 @@ export const BoardList = async () => {
       createdAt: "desc",
     },
   });
+
+  const availableCount = await getAvailableCount();
 
   return (
     <div className="space-y-4">
@@ -51,7 +55,9 @@ export const BoardList = async () => {
             className="aspect-video relative h-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
+            <span className="text-xs">{`${
+              MAX_FREE_BOARDS - availableCount
+            } remaining`}</span>
 
             <Hint
               sideOffset={40}
