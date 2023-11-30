dd6ffb3d91c3ffd0f91008c0c11a565383079b66
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-30 11:11:47 +0000

    set title optional on card update

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -12,13 +12,15 @@ export const UpdateCard = z.object({
         message: "Description is too short.",
       })
   ),
-  title: z
-    .string({
-      required_error: "Title is required.",
-      invalid_type_error: "Title is required.",
-    })
-    .min(3, {
-      message: "Title is too short.",
-    }),
+  title: z.optional(
+    z
+      .string({
+        required_error: "Title is required.",
+        invalid_type_error: "Title is required.",
+      })
+      .min(3, {
+        message: "Title is too short.",
+      })
+  ),
   id: z.string(),
 });
