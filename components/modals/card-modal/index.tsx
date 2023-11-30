615c561d79755614da69d7c18ea47559deb0459c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-11-30 11:13:09 +0000

    descrption skeleton added

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "@tanstack/react-query";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 import { Header } from "@/components/modals/card-modal/header";
+import { Description } from "@/components/modals/card-modal/description";
+import { Actions } from "@/components/modals/card-modal/actions";
 import { useCardModal } from "@/hooks/use-card-modal";
 import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
@@ -23,6 +25,20 @@ export const CardModal = () => {
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
         {cardData ? <Header data={cardData} /> : <Header.Skeleton />}
+
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {cardData ? (
+                <Description data={cardData} />
+              ) : (
+                <Description.Skeleton />
+              )}
+            </div>
+          </div>
+
+          {cardData ? <Actions data={cardData} /> : <Actions.Skeleton />}
+        </div>
       </DialogContent>
     </Dialog>
   );
