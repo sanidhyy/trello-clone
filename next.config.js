2e4f5c2613b6e996e22fad81fca30f79724cc8ec
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-01 06:27:46 +0000

    [pwa] diasble pwa in dev server

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  // disable: !prod,
+  disable: !prod,
 });
 
 /** @type {import('next').NextConfig} */
