8784fb0c9f6745443d9185b5df05130f5f7512a1
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-01 04:41:29 +0000

    stripe webhook api added to middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/webhook"],
   afterAuth(auth, req) {
     if (auth.userId && auth.isPublicRoute) {
       let path = "/select-org";
