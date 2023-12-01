1be5b11cfd14b5f122b9b7fdf2cc5e1cbc8e62cf
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-01 05:43:03 +0000

    github link added

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Github } from "lucide-react";
 
 import { Logo } from "@/components/logo";
 import { buttonVariants } from "@/components/ui/button";
@@ -9,7 +10,7 @@ export const Navbar = () => {
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
 
-        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+        <div className="space-x-4 md:w-auto flex items-center justify-between w-full">
           <Link
             href="/sign-in"
             className={buttonVariants({ size: "sm", variant: "outline" })}
@@ -19,6 +20,15 @@ export const Navbar = () => {
 
           <Link href="/sign-up" className={buttonVariants({ size: "sm" })}>
             Get Taskify for free
+          </Link>
+
+          <Link
+            href="https://github.com/sanidhyy/trello-clone"
+            target="_blank"
+            rel="noreferrer noopener"
+            className={buttonVariants({ size: "sm" })}
+          >
+            <Github className="h-4 w-4" />
           </Link>
         </div>
       </div>
