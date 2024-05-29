36507e7039a8195bfc8df0794df91b3aded83f64
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-29 21:58:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { PropsWithChildren } from "react";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 
-import "./globals.css";
 import { siteConfig } from "@/config/site";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
