c08a54271fadb800c15cd3b3a7664b140413bfa5
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-08-28 18:36:26 +0530

    fix: disable vercel image optimization to save resources

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const withPWA = require("next-pwa")({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
